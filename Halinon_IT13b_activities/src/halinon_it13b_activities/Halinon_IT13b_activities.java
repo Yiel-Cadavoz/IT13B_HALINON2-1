9f86a4ca31382480e34971ca8a732339a628fdd0
ronabaldoza <>
2025-02-13 12:04:35 +0800

    Added Java code

---
+++ Halinon_IT13b_activities/src/halinon_it13b_activities/Halinon_IT13b_activities.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package halinon_it13b_activities;
+
+/**
+ *
+ * @author User
+ */
+public class Halinon_IT13b_activities {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Halinon_IT13b_activities/src/prelim/LAB_act1_arithmetic.java
@@ -0,0 +1,16 @@
+package prelim;
+
+public class LAB_act1_arithmetic {
+    public static void main (String[] args){
+        
+       int x = 10;
+       int y = 4;
+       int z = 6;
+       
+       System.out.println(x + "*" + y + " + " + z + " = "+ (x * y + z));
+       System.out.println(x + "-" + y + " % " + z + " = "+ ((x - y) % z)); 
+       System.out.println(x + "+" + y + " + " + z + " = "+ ((x + y + z) / 3));
+       System.out.println(x + "*" + z + " - " + y + " * " + y + " = " + (x * z - (y*y)));
+             
+    }
+}
